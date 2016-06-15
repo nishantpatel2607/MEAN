a7d5676fde7bcd5dfd52c98e8c8da1661c0f1985
nishantpatel2607 <npatel2607@gmail.com>
2016-06-15 11:02:57 +1000

    ..

--- api/routes/index.js
+++ api/routes/index.js
@@ -10,7 +10,9 @@ router
 
 router
 .route('/hotels/:hotelId')
-.get(ctrlHotels.hotelsGetOne);
+.get(ctrlHotels.hotelsGetOne)
+.put(ctrlHotels.hotelsUpdateOne)
+.delete(ctrlHotels.hotelsDeleteOne);
 
 
 
@@ -22,7 +24,9 @@ router
 
 router
 .route('/hotels/:hotelId/reviews/:reviewId')
-.get(ctrlReviews.reviewsGetOne);
+.get(ctrlReviews.reviewsGetOne)
+.put(ctrlReviews.reviewsUpdateOne)
+.delete(ctrlReviews.reviewsDeleteOne);
 
 
 module.exports = router;
